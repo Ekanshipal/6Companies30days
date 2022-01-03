eb0a4f2a4d6124526ce3b43a242453193c5f1a55
Ekanshipal <71301005+Ekanshipal@users.noreply.github.com>
2022-01-03 18:04:01 +0530

    Create Q2_OverlappingRectangles.cpp

---
+++ Goldman Sachs( Day 1-5 )/Q2_OverlappingRectangles.cpp
@@ -0,0 +1,8 @@
+int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        if(L2[0]>R1[0]||R2[0]<L1[0])
+        return false;
+        if(L1[1]<R2[1]||L2[1]<R1[1])
+        return false;
+        return true;
+    }
+    
