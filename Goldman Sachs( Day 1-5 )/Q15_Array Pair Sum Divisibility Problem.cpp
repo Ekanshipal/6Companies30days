448973ebf43c55aaf11d2a190dcb9c3d31d82a0b
Ekanshipal <71301005+Ekanshipal@users.noreply.github.com>
2022-01-04 23:56:20 +0530

    Create Q15_Array Pair Sum Divisibility Problem.cpp

---
+++ Goldman Sachs( Day 1-5 )/Q15_Array Pair Sum Divisibility Problem.cpp
@@ -0,0 +1,28 @@
+ bool canPair(vector<int> nums, int k) {
+       
+        int n = nums.size();
+    
+        if (n & 1) return false;
+        map<int, int> freq;
+        for (int i = 0; i < n; i++) freq[nums[i] % k]++;
+
+   
+        for (int i = 0; i < n; i++) {
+            
+            int rem = nums[i] % k;
+
+            if (2 * rem == k) {
+               
+                if (freq[rem] % 2 != 0) return false;
+            }
+
+            else if (rem == 0) {
+                if (freq[rem] & 1) return false;
+            }
+            else if (freq[rem] != freq[k - rem])
+                return false;
+        }
+        return true;
+    
+        
+    }
